9803595bdd682c34b5c752b55b39bd4443a52e05
DoYoung Ryu <dy8327@gmail.com>
2026-05-04 14:18:17 +0900

    add: 여러학생 입력받기

--- student_score.py
+++ student_score.py
@@ -8,67 +8,76 @@ def calculate_sum(*args):
 
 
 def main():
+    in_num = 0
+    student_list = []
+    avg_list=[]
     # 2. 사용자 입력 활용하기 (input 사용)
     # PDF 04-2 사용자 입출력 자료를 기반으로 프롬프트를 띄워 입력을 받습니다.
     print("=== 학생 성적 처리 시스템 ===")
     print("과목 별 점수는 공백으로 구분하여 넣어주십시요..")
     print("(순서는 국어 수학 영어 과학 사회 순 입니다. )")
-    student_data = input("이름 입력: ")
-    subject_data = ("국어 수학 영어 과학 사회")
-    score_data = input("점수 입력: ")
-    
+    while in_num != 1:
+        student_data = input("이름 입력: ")
+        subject_data = ("국어 수학 영어 과학 사회")
+        score_data = input("점수 입력: ")
+
     # 입력받은 문자열을 리스트로 변환
-    subject_list = subject_data.split()
-    score_list = score_data.split()
+        student_list.append(student_data)
+        subject_list = subject_data.split()
+        score_list = score_data.split()
+
+        in_num=int(input("계속입력=0, 입력중단=1 :"))
 
     # 3. 딕셔너리 활용
-    subject_board = {
-        "국어": int (score_list[0]),
-        "수학": int (score_list[1]),
-        "영어": int (score_list[2]),
-        "과학": int (score_list[3]),
-        "사회": int (score_list[4])
-    }
+        subject_board = {
+            "국어": int(score_list[0]),
+            "수학": int(score_list[1]),
+            "영어": int(score_list[2]),
+            "과학": int(score_list[3]),
+            "사회": int(score_list[4])
+        }
 
-    # 4. 리스트 조작 및 제어문: 주문된 메뉴의 가격들만 모으기
-    input_scores = []
-   
-    for item in subject_list:
-        if item in subject_board:
-            scores = subject_board[item]
-            input_scores.append(scores)
+        # 4. 리스트 조작 및 제어문: 주문된 메뉴의 가격들만 모으기
+        input_scores = []
+
+        for item in subject_list:
+            if item in subject_board:
+                scores = subject_board[item]
+                input_scores.append(scores)
+            else:
+                print(f"경고: '{item}'은(는) 없는 과목이므로 제외됩니다. (다시 입력해주세요.)")
+
+        # 5. 함수 호출: 가변 매개변수 함수에 리스트 내용 전달 (언패킹)
+        total_scores = calculate_sum(*input_scores)
+        total_avg = total_scores // len(input_scores)
+        avg_list.append(total_avg)
+
+        if total_avg >= 90:
+            grade = 'A'
+        elif total_avg >= 80:
+            grade = 'B'
+        elif total_avg >= 70:
+            grade = 'C'
+        elif total_avg >= 60:
+            grade = 'D'
         else:
-            print(f"경고: '{item}'은(는) 없는 과목이므로 제외됩니다. (다시 입력해주세요.)")
+            grade = 'F'
 
-    # 5. 함수 호출: 가변 매개변수 함수에 리스트 내용 전달 (언패킹)
-    total_scores = calculate_sum(*input_scores)
-    total_avg = total_scores // len(input_scores)
+        # 6. 파일 입출력 및 f-문자열 포매팅: 정산 리포트 생성
+        with open("c:/python/score_report.txt", "a", encoding="utf-8") as f:
+            f.write("--- 기말고사 성적 보고서 ---\n")
+            f.write(f"대상 학생 : {student_data}\n")
+            f.write(f"총 점수: {total_scores}점\n")
+            f.write(f"평균 점수 :{total_avg}점\n")
+            f.write(f"최종등급 : {grade}\n")
+            f.write("----------------------\n")
 
-    if total_avg>=90:
-        grade = 'A'
-    elif total_avg>=80:
-        grade = 'B'
-    elif total_avg>=70:
-        grade = 'C'
-    elif total_avg>=60:
-        grade = 'D'
-    else:
-        grade ='F'
-
-
-    # 6. 파일 입출력 및 f-문자열 포매팅: 정산 리포트 생성
-    with open("c:/python/score_report.txt", "w", encoding="utf-8") as f:
-        f.write("--- 기말고사 성적 보고서 ---\n")
-        f.write(f"대상 학생 : {student_data}\n")
-        f.write(f"총 점수: {total_scores}점\n")
-        f.write(f"평균 점수 :{total_avg}점\n")
-        f.write(f"최종등급 : {grade}\n")
-        f.write("----------------------\n")
+    for i in range(len(student_list)):
+         print(student_list[i], avg_list[i])
 
     print("\n" + "="*30)
     print(f"저장 위치 c:/pyton/score_report.txt 로 저장되었습니다.")
     print("="*30)
-
 
 
 # 프로그램 시작점 (Entry Point)

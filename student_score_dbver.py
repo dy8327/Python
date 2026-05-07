891d70ace8012d4b103f37a3491ba617c665f642
DoYoung Ryu <dy8327@gmail.com>
2026-05-07 16:35:53 +0900

    fix: 반복 및 종료 문제 해결

--- student_score_dbver.py
+++ student_score_dbver.py
@@ -32,8 +32,8 @@ def insert_score_data(cursor, tch_string):  # , score_db):
             print("입력된 과목 수가 다릅니다.")
             return
     except ValueError:
-            print("점수는 숫자만 입력해주세요.")
-            return
+        print("점수는 숫자만 입력해주세요.")
+        return
     total = sum(score)
     avg = round(total/len(score), 2)
     insert_sql = "INSERT INTO SCORE_BOX(STU_NAME, KOR, ENG, MATH, SOCI, SCIN, TOTAL, S_AVG) VALUES(:1, :2, :3, :4, :5, :6, :7, :8)"
@@ -57,7 +57,7 @@ def inquire_one_data(cursor, stu_name):  # 개별조회
     return cursor.fetchone()
 
 
-def insert_comment(cursor, comment, stu_name): #코멘트 입력
+def insert_comment(cursor, comment, stu_name):  # 코멘트 입력
     s_name = stu_name.strip()
     in_comment_sql = "UPDATE SCORE_BOX SET COMENT= :1 WHERE STU_NAME= :2"
     cursor.execute(in_comment_sql, (comment, s_name,))
@@ -74,76 +74,76 @@ def main():
     print("1. 교사")
     print("2. 학생")
     in_num = int(input("해당하는 번호를 입력하세요.\n"))
-    conti_num = 0
-    tch_num = 0
-    while True:
-        match in_num:
-            case 1:
-                print("1. 성적 입력")
-                print("2. 성적 조회")
-                print("3. 코멘트 작성")
-                print("4. 종료")
-                tch_num = int(input("원하는 메뉴를 선택하세요: "))
-                if tch_num == 1:
-                    while True:
-                        tch_input = input(
-                            "> 성적을 입력하세요(이름, 국어, 영어, 수학, 사회, 과학)\n")
-                        insert_score_data(cursor, tch_input)
-                        conn.commit()
-                        again = input("계속입력(y/n): ")
-                        if again.lower() != 'y':
-                            break
-                        
-                elif tch_num == 2:
-                    while True:
-                        print("1. 전체 조회")
-                        print("2. 개별 조회")
-                        print("3. 뒤로가기")
-                        sub = int(input("원하는 메뉴를 선택하세요: "))
-                        if sub == 1:
-                            all_data = inquire_all_data(cursor)
-                            for cls_total, cls_avg, stu_name in all_data:
-                                print(f"학급 총점{cls_total}점, 학급 평균{cls_avg}점")
-                                print(stu_name)
+# 교사 메뉴
+    if in_num == 1:
+        while True:
+            print("1. 성적 입력")
+            print("2. 성적 조회")
+            print("3. 코멘트 작성")
+            print("4. 종료")
+            tch_num = int(input("원하는 메뉴를 선택하세요: "))
+            if tch_num == 1:
+                while True:
+                    tch_input = input(
+                        "> 성적을 입력하세요(이름, 국어, 영어, 수학, 사회, 과학)\n")
+                    insert_score_data(cursor, tch_input)
+                    conn.commit()
 
-                        elif sub == 2:
-                            stu_name = input("학생 이름을 입력하세요: ")
-                            one_data = inquire_one_data(cursor, stu_name)
-                            if one_data is None:
-                                print("해당 학생이 없습니다.")
-                                continue
+                    again = input("계속입력(y/n): ")
+                    if again.lower() != 'y':
+                        break
+
+            elif tch_num == 2:
+                while True:
+                    print("1. 전체 조회")
+                    print("2. 개별 조회")
+                    print("3. 뒤로가기")
+                    sub = int(input("원하는 메뉴를 선택하세요: "))
+                    if sub == 1:
+                        all_data = inquire_all_data(cursor)
+                        for cls_total, cls_avg, stu_name in all_data:
+                            print(f"학급 총점{cls_total}점, 학급 평균{cls_avg}점")
+                            print(stu_name)
+
+                    elif sub == 2:
+                        stu_name = input("학생 이름을 입력하세요: ")
+                        one_data = inquire_one_data(cursor, stu_name)
+                        if one_data is None:
+                            print("해당 학생이 없습니다.")
+                        else:    
                             stu_name, kor, eng, math, soci, scin, coment = one_data
                             print(
                                 f"{stu_name} 국어 {kor}, 영어{eng}, 수학{math}, 사회{soci}, 과학{scin}")
                             print(f"선생님 코멘트 : {coment}")
 
-                        elif sub == 3:
-                            break
-                    
-                elif tch_num == 3:
-                    stu_name = input("학생 이름 : ")
-                    comment = input("코멘트를 작성해주세요(200자이내) : ")
-                    insert_comment(cursor, comment, stu_name)
-                    conn.commit()
-                    if cursor.rowcount == 0:
-                        print("해당 학생을 찾을 수 없습니다.")
-                    else:
-                        print("코멘트가 저장되었습니다.")
+                    elif sub == 3:
+                        break
 
-                
-                elif tch_num == 4:
-                    print("프로그램 종료")
-                    break
-            case 2:
-                stu_name = input("학생 이름을 입력하세요: ")
-                one_data = inquire_one_data(cursor, stu_name)
-                if one_data is None:
-                    print("해당 학생이 없습니다.")
-                    continue
-                stu_name, kor, eng, math, soci, scin, coment = one_data
-                print(f"{stu_name} 국어 {kor}, 영어{eng}, 수학{math}, 사회{soci}, 과학{scin}")
-                print(f"선생님 코멘트 : {coment}")
-                return
+            elif tch_num == 3:
+                stu_name = input("학생 이름 : ")
+                comment = input("코멘트를 작성해주세요(200자이내) : ")
+                insert_comment(cursor, comment, stu_name)
+                conn.commit()
+
+                if cursor.rowcount == 0:
+                    print("해당 학생을 찾을 수 없습니다.")
+                else:
+                    print("코멘트가 저장되었습니다.")
+
+            elif tch_num == 4:
+                print("프로그램 종료")
+                break
+    #학생메뉴
+    elif 2:
+        stu_name = input("학생 이름을 입력하세요: ")
+        one_data = inquire_one_data(cursor, stu_name)
+        if one_data is None:
+            print("해당 학생이 없습니다.")
+        else:    
+            stu_name, kor, eng, math, soci, scin, coment = one_data
+            print(f"{stu_name} 국어 {kor}, 영어{eng}, 수학{math}, 사회{soci}, 과학{scin}")
+            print(f"선생님 코멘트 : {coment}")
+            return
 
 
 if __name__ == "__main__":
